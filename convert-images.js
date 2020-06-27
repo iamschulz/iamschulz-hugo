b72c44c123210779674372fa2e7e2e345c0f112d
Daniel Schulz <daniel.schulz@spiegel.de>
2020-06-27 17:08:52 +0200

    improve webp converter

--- convert-images.js
+++ convert-images.js
@@ -1,43 +1,106 @@
+// import dependencies
 const fs = require('fs');
 const mime = require('mime-types');
 const webp = require('webp-converter');
-const testFolder = './dist';
 
-//pass input image(.jpeg,.pnp .....) path ,output image(give path where to save and image file name with .webp extension)
-//pass option(read  documentation for options)
+// globals
+const consoleLog = console.log;
+const images = [];
+const gifs = [];
 
-//cwebp(input,output,option,result_callback)
-
-webp.cwebp("input.jpg", "output.webp", "-q 80", function (status, error) {
-    //if conversion successful status will be '100'
-    //if conversion fails status will be '101'
-    console.log(status, error);
+// parse args
+const config = {};
+process.argv.filter(cnf => cnf.includes('=')).forEach((cnf) => {
+    config[cnf.split('=')[0]] = cnf.split('=')[1];
 });
+const dir = config.dir ? config.dir : './public';
+const override = config.override ? config.override : false;
+
+// methods
+const getWebpPath = (path) => {
+    let splitPath = path.split(".");
+    splitPath.pop();
+    return splitPath.join(".") + ".webp";
+}
 
 const crawlDir = (dir) => {
     fs.readdirSync(dir).forEach(file => {
         const path = `${dir}/${file}`;
         const isDir = fs.lstatSync(path).isDirectory();
         const mimetype = mime.lookup(path);
+        const webpPath = getWebpPath(path);
+        const webpExists = fs.existsSync(webpPath) && !override;
 
-        if (mimetype && mimetype.split('/')[0] === 'image') {
+        if (
+            (!webpExists) && (
+                mimetype && mimetype.split('/')[1] === 'jpeg'
+                || mimetype && mimetype.split('/')[1] === 'png'
+            )
+        ) {
             images.push(path);
+        } else if (
+            (!webpExists) && (
+                mimetype && mimetype.split('/')[1] === 'gif'
+            )
+        ) {
+            gifs.push(path);
         } else if (isDir) {
             crawlDir(path);
         }
     });
 }
 
-const images = [];
-crawlDir(testFolder);
-images.forEach(image => {
+const enableLog = () => { console.log = consoleLog };
+const disableLog = () => { console.log = function() {} };
 
-    webp.cwebp(image, "./output.webp", "-q 100", function (status, error) {
+const convertImage = image => {
+    const webpPath = getWebpPath(image);
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`converting ${image}`);
+
+    disableLog();
+    webp.cwebp(image, webpPath, "-q 85", function (status, error) {
 
         //if conversion successful status will be '100'
         //if conversion fails status will be '101'
         if (status > 100) {
-            console.error(image, 'failed to convert');
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            enableLog();
+            console.error('failed to convert', image);
+            disableLog();
         }
     });
-})
+    enableLog();
+}
+
+const convertGif = image => {
+    const webpPath = getWebpPath(image);
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`converting ${image}`);
+
+    disableLog();
+    webp.gwebp(image, webpPath, "-q 85", function (status, error) {
+
+        //if conversion successful status will be '100'
+        //if conversion fails status will be '101'
+        if (status > 100) {
+            enableLog();
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            console.error('failed to convert', image);
+            disableLog();
+        }
+    });
+    enableLog();
+}
+
+// exec
+crawlDir(dir);
+console.log(`found ${images.length + gifs.length} files`);
+images.forEach((image) => {convertImage(image)});
+gifs.forEach((image) => {convertGif(image)});
